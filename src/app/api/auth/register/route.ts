303e28d7aa2122c0fe54691a3787d33fd68c7fce
Maximiliano Santana <iimaxsany@gmail.com>
2025-09-22 20:21:20 -0600

    Update: Api registro funcionando con todas las posibilidades de registro

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,30 +3,19 @@ import { validateRequest } from "@/app/api/lib/validation";
 import { NextRequest, NextResponse } from "next/server";
 import db from "@/lib/prisma";
 import bcrypt from "bcryptjs";
+import { TenantRole } from "@prisma/client";
 export async function POST(request: NextRequest) {
   //Validación de datos
   const newUser: RegisterDTO = await request.json();
   const validation = validateRequest(RegisterSchema, newUser);
   let invitation;
   let tenant;
+  let user;
   if (!validation.success) {
     return NextResponse.json({ message: validation.message }, { status: 400 });
   }
 
-  //Validar que el usuario no exista
-  const emailFound = await db.user.findUnique({
-    where: {
-      email: newUser.email,
-    },
-  });
-  if (emailFound) {
-    return NextResponse.json(
-      { message: "Usuario ya registrado" },
-      { status: 409 }
-    );
-  }
-
-  //Validar que el tenant exista si viene relacionado
+  //Validacion de tenant
   if (newUser.tenantId) {
     tenant = await db.tenant.findUnique({
       where: {
@@ -39,7 +28,7 @@ export async function POST(request: NextRequest) {
     }
   }
 
-  //Validar que el invite exista y sea valida
+  //Validacion de invite
   if (newUser.invitation) {
     invitation = await db.invitation.findUnique({
       where: {
@@ -72,80 +61,141 @@ export async function POST(request: NextRequest) {
     }
   }
 
-  //Se crea nuevo usuario
-  const user = await db.user.create({
-    data: {
-      name: newUser.name,
+  //Validacion de usuario
+  user = await db.user.findUnique({
+    where: {
       email: newUser.email,
-      password: await bcrypt.hash(newUser.password, 10),
-    },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      createdAt: true,
     },
   });
 
-  if (user) {
-    //Se crea relación tenant user
-    if (invitation && tenant) {
-      const tenantUser = await db.tenantUser.create({
-        data: {
-          tenantId: tenant.id,
+  //Si usuario global existía se termina registro
+  if (user && !invitation && !tenant) {
+    return NextResponse.json(
+      { message: "Usuario en Gym&i ya registrado." },
+      { status: 409 }
+    );
+  }
+
+  //Si usuario no existe se crea
+  if (!user) {
+    user = await db.user.create({
+      data: {
+        name: newUser.name,
+        email: newUser.email,
+        password: await bcrypt.hash(newUser.password, 10),
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+    console.log("Usuario creado", user);
+  }
+
+  //Si solo es registro global se retorna respuesta
+  if (user && !tenant && !invitation) {
+    return NextResponse.json(
+      { message: "Usuario en Gym&i creado correctamente." },
+      { status: 200 }
+    );
+  }
+
+  //Si hay invitacion crea/actualiza relacion
+  if (invitation) {
+    //Validacion de relación
+    const tenantUser = await db.tenantUser.findUnique({
+      where: {
+        userId_tenantId: {
           userId: user.id,
+          tenantId: invitation.tenantId,
+        },
+      },
+    });
+    //NO existe relación se crea
+    if (!tenantUser) {
+      await db.tenantUser.create({
+        data: {
+          userId: user.id,
+          tenantId: invitation.tenantId,
           roles: [invitation.role],
-          status: "ACTIVE",
         },
       });
-      if (!tenantUser) {
-        return NextResponse.json(
-          {
-            message: "Ocurrió un error al asignar el usuario a su tenant",
-            user,
-          },
-          { status: 500 }
-        );
-      }
-      await db.invitation.update({
+      //SI existe relacion se actualiza
+    } else {
+      const mergedRoles = Array.from(
+        new Set([...(tenantUser.roles ?? []), invitation.role])
+      );
+      await db.tenantUser.update({
         where: {
-          id: invitation.id,
-        },
-        data: {
-          usedAt: new Date(),
-        },
-      });
-      return NextResponse.json({
-        message: "Usuario creado correctamente",
-        user,
-      });
-    } else if (tenant) {
-      const tenantUser = await db.tenantUser.create({
-        data: {
-          tenantId: tenant.id,
-          userId: user.id,
-          roles: ["MEMBER"],
-          status: "ACTIVE",
-        },
-      });
-      if (!tenantUser) {
-        return NextResponse.json(
-          {
-            message: "Ocurrió un error al asignar el usuario a su tenant",
-            user,
+          userId_tenantId: {
+            userId: tenantUser.userId,
+            tenantId: tenantUser.tenantId,
           },
-          { status: 500 }
-        );
-      }
-      return NextResponse.json({
-        message: "Usuario creado correctamente",
-        user,
+        },
+        data: { roles: { set: mergedRoles } },
       });
     }
-  } else {
+
+    //Se marca invitación como usada
+    await db.invitation.update({
+      where: {
+        id: invitation.id,
+      },
+      data: {
+        usedAt: new Date(),
+      },
+    });
+
     return NextResponse.json(
-      { message: "Error creating user" },
-      { status: 500 }
+      { message: `Usuario creado correctamente por invitación.` },
+      { status: 200 }
+    );
+  }
+
+  //Si hay tenant se crea/actualiza relación
+  if (tenant && !invitation) {
+    //Valida existencia de relación
+    const tenantUser = await db.tenantUser.findUnique({
+      where: {
+        userId_tenantId: {
+          userId: user.id,
+          tenantId: tenant.id,
+        },
+      },
+    });
+    //NO existe relacion se crea
+    if (!tenantUser) {
+      await db.tenantUser.create({
+        data: {
+          userId: user.id,
+          tenantId: newUser.tenantId!,
+          roles: ["MEMBER"],
+        },
+      });
+      //SI existe relación se actualiza
+    } else {
+      const mergedRoles = [
+        ...new Set<TenantRole>([
+          ...(tenantUser.roles ?? []),
+          TenantRole.MEMBER,
+        ]),
+      ];
+
+      await db.tenantUser.update({
+        where: {
+          userId_tenantId: {
+            userId: tenantUser.userId,
+            tenantId: tenantUser.tenantId,
+          },
+        },
+        data: { roles: { set: mergedRoles } },
+      });
+    }
+    return NextResponse.json(
+      { message: `Usuario creado correctamente en ${tenant.name}.` },
+      { status: 200 }
     );
   }
 }
